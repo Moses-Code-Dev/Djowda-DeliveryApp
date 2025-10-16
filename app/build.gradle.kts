82c1729197fb1ea333b46bfd01a32a811c727fdb
Moses-Code-Dev <71468750+Moses-Code-Dev@users.noreply.github.com>
2025-10-16 11:55:32 +0100

    clean-update-project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.djowda.djowdaDelivery"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.djowda.djowdaDelivery"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
